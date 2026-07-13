194a331d9a9519d4d6e7375702de7d4804d91006
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-07-13 15:32:56 +0300

    feat: add user response dto for itself

---
+++ Vendora/Application/DTO/UserDTO/UserResponseForItselfDTO.cs
@@ -0,0 +1,20 @@
+﻿using _user;
+namespace _userDTO
+{
+    public record UserResponseForItselfDTO(
+        string ProfileName,
+        string FirstName,
+        string LastName,
+        string Gender,
+        string? AvatarUrl,
+        string? Email,
+        string? Phone,
+        decimal Balance
+    )
+    {
+        public UserResponseForItselfDTO(User user) :
+            this(user.ProfileName, user.FirstName, user.LastName, user.UserGender.ToString(),
+                user.AvatarUrl, user.Email, user.Phone, user.Balance)
+        { }
+    };
+}
