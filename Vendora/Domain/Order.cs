540956ed5d2ecbe6eb60ba9f263c9d28c4712fe5
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-07-16 20:30:34 +0300

    ffeat: add Order and OrderItem

---
+++ Vendora/Domain/Order.cs
@@ -0,0 +1,28 @@
+﻿using Domain.OrderItems;
+using Domain.Users;
+namespace Domain.Orders
+{
+    public class Order
+    {
+        public Ulid Id { get; set; }
+
+        public Ulid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public decimal TotalPrice { get; set; }
+        public enum OrderStatus
+        {
+            Created,
+            Paid,
+            Processing,
+            Shipped,
+            Delivered,
+            Cancelled
+        }
+        public OrderStatus Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; } = [];
+    }
+}
---
+++ Vendora/Domain/OrderItemcs.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Orders;
+using Domain.Products;
+namespace Domain.OrderItems
+{
+    public class OrderItem
+    {
+        public Ulid Id { get; set; }
+
+        public Ulid OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+
+        public Ulid ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
