6be42575e16488638327db91e32b3a383f6f4010
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-07-24 13:16:39 +0300

    feat: add CartResponseDTO

---
+++ Vendora/Application/DTO/CartDTO/СartResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTO.ProductDTO.CartDTO;
+using Domain.Carts;
+namespace Application.DTO.CartDTO
+{
+    public record CartResponseDTO(Ulid Id,
+        Ulid UserId,
+        List<ProductCartCardResponseDTO> cartItems,
+        int TotalQuantity,
+        decimal TotalPrice
+        )
+    {
+        public CartResponseDTO(Cart cart) :
+            this(cart.Id,
+                cart.UserId,
+                cart.Items.Select(x => new ProductCartCardResponseDTO(x)).ToList(),
+                cart.Items.Sum(x => x.Quantity),
+                cart.Items.Sum(x => x.Quantity * x.PricePerUnit))
+        { }
+    }
+}
