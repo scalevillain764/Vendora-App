13325d2b81a69f3991c303aae405b71660a871f3
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-07-24 12:36:11 +0300

    feat: add CartController

--- Vendora/Presentation/Controllers/AuthController.cs
+++ Vendora/Presentation/Controllers/AuthController.cs
@@ -33,8 +33,8 @@ namespace Presentation.Controllers
         [Route("refresh")]
         public async Task<IActionResult> RefreshAsync()
         {
-            var userId = GetUserId();
-            var rez = await _authService.RefreshAsync(userId);
+            var ULID_userId = GetUserId();
+            var rez = await _authService.RefreshAsync(ULID_userId);
             return ProcessResult(rez);
         }
     }
---
+++ Vendora/Presentation/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ICartService = Application.Interfaces.ICartService;
+using Application.DTO.ProductDTO.CartDTO;
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : BaseController
+    {
+        private readonly ICartService _cartService;
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyCartAsync()
+        {
+            var ULID_userId = GetUserId();
+            var rez = await _cartService.GetMyCartAsync(ULID_userId);
+            return ProcessResult(rez);
+        }
+
+        [HttpPost]
+        [Route("items/{cartItemId}/increase")]
+        public async Task<IActionResult> IncreaseQuantityAsync(Ulid cartItemId)
+        {    
+            var ULID_userId = GetUserId();
+            var rez = await _cartService.IncreaseQuantityAsync(ULID_userId, cartItemId);
+            return ProcessResult(rez);
+        }
+
+        [HttpPost]
+        [Route("items/{cartItemId}/dencrease")]
+        public async Task<IActionResult> DecreaseQuantityAsync(Ulid cartItemId)
+        {
+            var ULID_userId = GetUserId();
+            var rez = await _cartService.DecreaseQuantityAsync(ULID_userId, cartItemId);
+            return ProcessResult(rez);
+        }
+
+        [HttpDelete]
+        [Route("items/{cartItemId}")]
+        public async Task<IActionResult> RemoveCartItemAsync(Ulid cartItemId)
+        {
+            var ULID_userId = GetUserId();
+            var rez = await _cartService.RemoveCartItemAsync(ULID_userId, cartItemId);
+            return ProcessResult(rez);
+        }
+    }
+}
