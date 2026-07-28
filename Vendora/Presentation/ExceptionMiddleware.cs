6d04d91770379de0f691feb2e2beac271bf63be0
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-07-28 15:51:11 +0300

    feat: add ExceptionMiddleWare

---
+++ Vendora/Presentation/ExceptionMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace Presentation.ExceptionMiddlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.StatusCode = 500;
+                httpContext.Response.ContentType = "application/json";
+                _logger.LogError(ex.Message);
+                await httpContext.Response.WriteAsJsonAsync(new
+                {
+                    Success = false,
+                    Message = "Произошла внутренняя ошибка сервера."
+                });
+            }
+        }
+    }
+}
