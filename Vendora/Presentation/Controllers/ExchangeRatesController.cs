6a8603749492d9b38e8ad9862900e109f68beb6e
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-07-29 16:01:36 +0300

    feat: add ExchangeRatesController

---
+++ Vendora/Presentation/Controllers/ExchangeRatesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using IExchangeRatesService = Application.Interfaces.IExchangeRateService;
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class ExchangeRatesController : BaseController
+    {
+        private readonly IExchangeRatesService _service;
+        public ExchangeRatesController(IExchangeRatesService service) {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetExchangeRatesAsync() => ProcessResult(await _service.GetExchangeRatesAsync());
+    }
+}
