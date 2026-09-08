02144b42c81126f6300833e593dcf0d34928d45d
scalevillain764 <kvyatkovskymaxim7@gmail.com>
2026-09-08 08:58:22 +0300

    fix: fix bug in productCardCartResponse

--- Vendora/Application/DTO/ProductDTO/CartDTO/ProductCartCardResponseDTO.cs
+++ Vendora/Application/DTO/ProductDTO/CartDTO/ProductCartCardResponseDTO.cs
@@ -7,7 +7,6 @@ namespace Application.DTO.ProductDTO.CartDTO
         decimal PricePerUnit,
         string? ShortDescription,
         string? PreviewUrl,
-        decimal PricePerUnit,
         int Quantity)
     {
         public ProductCartCardResponseDTO(Product product, int quantity)
